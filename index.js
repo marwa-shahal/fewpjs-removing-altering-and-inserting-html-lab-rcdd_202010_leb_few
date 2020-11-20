bf958a8e014d434984d2904ccb946e28038d2f08
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-20 23:02:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+const elem = document.querySelector('#main');
+elem.parentNode.removeChild(elem);
+//has a newheader variable that points to node 'h1#victory'
+
+    
+    
+    
+    const newHeader=document.createElement('h1');
+     newHeader.id='victory';
+     newHeader.setAttribute= ("id","victory");
+      newHeader.innerHTML=" YOUR-NAME is the champion";
+     document.body.appendChild(newHeader);
+     
+    
+    
+    
+ 
